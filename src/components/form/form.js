e07f42ab51af4c80010a19cbadc6a586a5a49ac3
Tyler Steffensen <stef0041@algonquinlive.com>
2019-02-08 13:59:00 -0500

    Finishing contact form

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,6 +1,61 @@
 export default {
-  name: 'form',
-  props: {
-    msg: String
+  data() {
+    return {
+      dialogVisible: false,
+      claimForm: {
+        name: '',
+        lastName: '',
+        email: '',
+        subject: '',
+        textArea: '',
+        terms: ''
+      },
+      rules: {
+        name: [{
+          required: true,
+          message: 'Please input First Name',
+          trigger: 'blur'
+        }],
+        lastName: [{
+          required: true,
+          message: 'Please input Lsst Name',
+          trigger: 'blur'
+        }],
+        email: [{
+          required: true,
+          message: 'Please input Email',
+          trigger: 'blur'
+        }],
+        subject: [{
+          required: true,
+          message: 'Please input Subject',
+          trigger: 'blur'
+        }],
+        textArea: [{
+          required: true,
+          message: 'Please input some Text',
+          trigger: 'blur'
+        }],
+        terms: [{
+          required: true,
+          message: 'Please Agree to my terms',
+          trigger: 'change'
+        }]
+      }
+
+    }
+  },
+  methods: {
+    submitForm(claimForm) {
+      this.$refs[claimForm].validate((valid) => {
+        if (valid) {
+          this.dialogVisible = true;
+        } else {
+          /* eslint-disable */
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    }
   }
 }
--- src/components/form/form.vue
+++ src/components/form/form.vue
@@ -1,76 +1,52 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,
-      <br>check out the
-      <a
-        href="https://cli.vuejs.org"
-        target="_blank"
-        rel="noopener"
-      >vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-        >babel</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-        >eslint</a>
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a>
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a>
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a>
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a>
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-        >vue-devtools</a>
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a>
-      </li>
-      <li>
-        <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a>
-      </li>
-    </ul>
+    <el-form :model="claimForm" :rules="rules" ref="claimForm">
+      <el-form-item label="First Name" prop="name">
+        <el-input placeholder="Enter First Name" v-model="claimForm.name"></el-input>
+      </el-form-item>
+
+      <el-form-item label="Last Name" prop="lastName">
+        <el-input placeholder="Enter Last Name" v-model="claimForm.lastName"></el-input>
+      </el-form-item>
+
+      <el-form-item label="Email" prop="email">
+        <el-input placeholder="Enter Email" v-model="claimForm.email"></el-input>
+      </el-form-item>
+
+      <el-form-item label="Subject" prop="subject">
+        <el-input placeholder="Enter Subject" v-model="claimForm.subject"></el-input>
+      </el-form-item>
+
+      <el-form-item label="Text Area" prop="textArea">
+        <el-input type="textarea" placeholder="Please input text fool" v-model="claimForm.textArea"></el-input>
+      </el-form-item>
+
+      <el-form-item label="Terms and Conditions" prop="terms">
+        <el-checkbox v-model="claimForm.terms">Sell me your soul!</el-checkbox>
+      </el-form-item>
+
+      <el-row>
+        <el-button type="primary" @click="submitForm('claimForm')">Submit</el-button>
+      </el-row>
+
+      <el-dialog title="Confirm Information" :visible.sync="dialogVisible" width="30%">
+        {{claimForm.name}}
+        <br>
+        {{claimForm.lastName}}
+        <br>
+        {{claimForm.email}}
+        <br>
+        {{claimForm.subject}}
+        <br>
+        {{claimForm.textArea}}
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="dialogVisible = false">Cancel</el-button>
+          <el-button type="primary" @click="dialogVisible = false">Confirm</el-button>
+        </span>
+      </el-dialog>
+    </el-form>
   </div>
 </template>
 
 <script src="./form.js"></script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style src="./form.css" scoped></style>
